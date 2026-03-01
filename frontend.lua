cc714fc8778b4b51e7cd088ba583d6cd0466305f
Karol <Karol@cockandball.es>
2026-03-01 15:31:59 +0000

    finished

--- frontend.lua
+++ frontend.lua
@@ -1,15 +1,99 @@
 local player_timer_util = require("player_timer")
 local term = require("term")
-local task = require("task")
+local os = require("os")
+local math = require("math")
+-- gpu
+local component = require("component")
+local gpu = component.gpu
+local screen = component.screen
+gpu.setResolution(60, 16)
 
-print("Starting Player Timer...")
+-- Change the background and foreground
+gpu.setBackground(0x000000) -- black
+gpu.setForeground(0xFFFFFF) -- white
+
+--print("Starting Player Timer...")
 player_timer_util.init()
-while true do
-    local player_table = player_timer_util.PlayerWatchLoop()
-    print("Player Table:")
-    for name, time in pairs(player_table) do
-        print(name .. " = " .. time)
+
+-- Set background color
+gpu.setBackground(0x000000)
+-- Fill the entire screen with spaces
+local w, h = gpu.getResolution()
+gpu.fill(1, 1, w, h, " ")
+
+-- func declare
+local function formatTime(seconds)
+    local hours = math.floor(seconds / 3600)
+    local minutes = math.floor((seconds % 3600) / 60)
+    local secs = seconds % 60
+    return string.format("%d hours %d minutes %d seconds", hours, minutes, secs)
+end
+
+local function drawBar(x, y, width, height, value, maxValue, bgColor, fillColor, text)
+    -- Draw background
+    gpu.setBackground(bgColor)
+    for i = 0, height-1 do
+        gpu.fill(x, y+i, width, 1, " ")
     end
-    print("------------------------------")
-    task.wait(1)
+
+    -- Draw fill
+    local fillWidth = math.floor((value / maxValue) * width)
+    gpu.setBackground(fillColor)
+    for i = 0, height-1 do
+        gpu.fill(x, y+i, fillWidth, 1, " ")
+    end
+
+    -- Draw text above
+    gpu.setBackground(0x000000)
+    gpu.setForeground(0xFFFFFF)
+    gpu.set(x, y-1, text .. "                                        ")
+    if text == "Karol" then
+        gpu.set(x, y-1, text .. ":         " .. formatTime(value)) -- my name 1 char longer than rory and jack
+    else
+        gpu.set(x, y-1, text .. ":          " .. formatTime(value))
+    end
+    --gpu.set(x, y-1, text .. ":          " .. formatTime(value))
+end
+
+local function drawSquare(x, y, size, colour)
+    gpu.setBackground(colour)
+    gpu.fill(x, y, size+1, size, " ")
+end
+
+while true do
+    local player_table, player_clock_status = player_timer_util.PlayerWatchLoop()
+    local max_value = math.max(player_table["Karol"], player_table["Rory"], player_table["Jack"])
+    drawBar(1,3,59,2, player_table["Karol"], max_value, 0x303030, 0x00FF00, "Karol")
+    drawBar(1,6,59,2, player_table["Rory"], max_value, 0x303030, 0x00FF00, "Rory")
+    drawBar(1,9,59,2, player_table["Jack"], max_value, 0x303030, 0x00FF00, "Jack")
+
+    for name, clock_state in pairs(player_clock_status) do
+        if clock_state then
+            if name == "Karol" then
+                drawSquare(17, 16, 2, 0x00FF00)
+            elseif name == "Rory" then
+                drawSquare(25, 16, 2, 0x00FF00)
+            elseif name == "Jack" then
+                drawSquare(34, 16, 2, 0x00FF00)
+            end
+        else
+            if name == "Karol" then
+                drawSquare(17, 16, 2, 0xFF0000)
+            elseif name == "Rory" then
+                drawSquare(25, 16, 2, 0xFF0000)
+            elseif name == "Jack" then
+                drawSquare(34, 16, 2, 0xFF0000)
+            end
+        end
+    end
+    drawSquare(42, 16, 2, 0xFF0000)
+    --drawSquare(17, 16, 2, 0x00FF00)
+    --drawSquare(25, 16, 2, 0x00FF00)
+    --drawSquare(34, 16, 2, 0x00FF00)
+    -- print("Player Table:")
+    -- for name, time in pairs(player_table) do
+    --     print(name .. " = " .. time)
+    -- end
+    -- print("------------------------------")
+    os.sleep(1)
 end
--- player_timer.lua
+++ player_timer.lua
@@ -5,76 +5,8 @@ local redstone = component.redstone
 local player_timer_util = {}
 local playerTimes = {}
 local player_clock_tracker = {}
--- forward declared funcs
-local is_player_clocked_in
-local clock_player
-local save_data
 
-function player_timer_util.init()
-    -- initialize player_time.txt if it doesn't exist
-    local file = io.open("player_time.txt", "r")
-    if not file then
-        file = io.open("player_time.txt", "w")
-        file:write("Karol:0=0\n")
-        file:write("Rory:0=0\n")
-        file:write("Jack:0=0\n")
-        file:close()
-    else
-        file:close()
-    end
-
-    -- populate playerTimes and player_clock_tracker from player_time.txt
-    file = io.open("player_time.txt", "r")
-    for line in file:lines() do
-        local player_name, clock_state, time = string.match(line, "^(%w+):(%d+)=(%d+)$")
-        player_clock_tracker[player_name] = clock_state == "1"
-        playerTimes[player_name] = tonumber(time)
-    end
-end
-
-function player_timer_util.PlayerWatchLoop()
-  -- watch for redstone signals for player clock in/out
-  -- this func runs 1 per second 
-
-    local signal = redstone.getInput(2) -- 2 = back side
-
-    -- find out which player is clocking in or out based on signal strength
-    local player_clocked = ""
-    local player_current_clock_state = false
-
-    if signal == 8 then
-        player_clocked = "Karol"
-    elseif signal == 9 then
-        player_clocked = "Rory"
-    elseif signal == 6 then
-        player_clocked= "Jack"
-    else
-        print("no one detected wtf do i even do here??")
-        return -1
-    end
-
-    print("Player activity detected: " .. player_clocked)
-    if is_player_clocked_in(player_clocked) then
-        print(player_clocked .. "is clockeded in")
-        player_current_clock_state = true
-    else
-        print(player_clocked .. "is clocked out")
-        player_current_clock_state = false
-    end
-
-    -- change players clock state
-    clock_player(not player_current_clock_state, player_clocked)
-
-    -- update player times
-    for name, clocked_in in pairs(player_clock_tracker) do
-        if clocked_in then
-            playerTimes[name] = (playerTimes[name] or 0) + 1
-        end
-    end
-    save_data()
-    return playerTimes
-end
-
+-- local funcs delcared here
 local function is_player_clocked_in(name)
     local file = io.open("player_time.txt", "r")
     if not file then
@@ -93,23 +25,29 @@ local function is_player_clocked_in(name)
     return false
 end
 
-local function clock_player(new_clock_state, player_joined)
+local function clock_player(new_clock_state, target_player_name)
     -- read all of player_time.txt and store it in a table
     local file = io.open("player_time.txt", "r")
     if file then
         for line in file:lines() do
             local player_name, clock_state, time = string.match(line, "^(%w+):(%d+)=(%d+)$")
             
-            -- debug
-            if new_clock_state == true and player_name == player_joined then
-                print(player_joined .. " clocked in")
-            elseif new_clock_state == false and player_name == player_joined then
-                print(player_joined .. " clocked out")
+            if(player_name ~= target_player_name) then
+                goto end_func
             end
 
+            -- debug
+            -- if new_clock_state == true and player_name == target_player_name then
+            --     print(player_name .. " clocked in")
+            -- elseif new_clock_state == false and player_name == target_player_name then
+            --     print(player_name .. " clocked out")
+            -- end
+
             -- log players new clock state and current time in time.txt
-            player_clock_tracker[player_name] = new_clock_state
-            playerTimes[player_name] = tonumber(time)
+            player_clock_tracker[target_player_name] = new_clock_state
+            playerTimes[target_player_name] = tonumber(time)
+
+            ::end_func::
         end
         file:close()
     end
@@ -127,6 +65,80 @@ local function save_data()
         end
     end
     file:close()
+end
+
+-- global funcs declared here
+
+function player_timer_util.init()
+    -- initialize player_time.txt if it doesn't exist
+    local file = io.open("player_time.txt", "r")
+    if not file then
+        file = io.open("player_time.txt", "w")
+        file:write("Karol:0=0\n")
+        file:write("Rory:0=0\n")
+        file:write("Jack:0=0\n")
+        file:close()
+    else
+        file:close()
+    end
+
+    -- populate playerTimes and player_clock_tracker from player_time.txt
+    file = io.open("player_time.txt", "r")
+    for line in file:lines() do
+        local player_name, clock_state, time = string.match(line, "^(%w+):(%d+)=(%d+)$")
+        --print("Initializing player: " .. player_name .. ", clock state: " .. clock_state .. ", time: " .. time)
+        player_clock_tracker[player_name] = clock_state == "1"
+        playerTimes[player_name] = tonumber(time)
+    end
+end
+function player_timer_util.PlayerWatchLoop()
+  -- watch for redstone signals for player clock in/out
+  -- this func runs 1 per second 
+
+    local signal = redstone.getInput(2) -- 2 = back side
+
+    -- before checking for inputs, increment all clocked in players
+    for name, clocked_in in pairs(player_clock_tracker) do
+        --print(name .. " clocked state: " .. tostring(clocked_in))
+        if clocked_in then
+            playerTimes[name] = (playerTimes[name] or 0) + 1
+        end
+    end
+    save_data()
+
+    -- find out which player is clocking in or out based on signal strength
+    local player_clocked = ""
+    local player_current_clock_state = false
+
+    -- debug
+    --print("Redstone signal detected: " .. signal)
+
+    if signal == 8 then
+        player_clocked = "Karol"
+    elseif signal == 9 then
+        player_clocked = "Rory"
+    elseif signal == 6 then
+        player_clocked= "Jack"
+    else
+        return playerTimes, player_clock_tracker
+    end
+
+    -- debug
+    --print("Player activity detected: " .. player_clocked)
+    if is_player_clocked_in(player_clocked) then
+        --print(player_clocked .. "is clockeded in")
+        player_current_clock_state = true
+    else
+        --print(player_clocked .. "is clocked out")
+        player_current_clock_state = false
+    end
+
+    -- change players clock state
+    clock_player(not player_current_clock_state, player_clocked)
+    -- update player times
+    save_data()
+
+    return playerTimes, player_clock_tracker
 end
 
 return player_timer_util
